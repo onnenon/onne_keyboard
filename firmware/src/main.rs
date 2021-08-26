584130ce8e2481d6eacfcf3087e2629be19c12a0
Stephen Onnen <stephen.onnen@gmail.com>
2021-08-25 21:38:12 -0500

    Working keyboard

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,66 +1,31 @@
 #![no_std]
 #![no_main]
 
-use bsp::hal::gpio::v2::Pin;
-use generic_array::typenum::U1;
-use keyberon::impl_heterogenous_array;
-use keyberon::key_code::KbHidReport;
 use panic_halt as _;
-
-use core::convert::Infallible;
-use embedded_hal::digital::v2::{InputPin, OutputPin};
 
 use bsp::hal;
 use bsp::pac;
 use qt_py_m0 as bsp;
 
+use bsp::entry;
 use cortex_m::asm::wfi;
 use cortex_m::peripheral::NVIC;
-use usb_device::bus::UsbBusAllocator;
-use usb_device::prelude::*;
-
-use bsp::entry;
 use hal::clock::GenericClockController;
 use hal::usb::UsbBus;
 use pac::interrupt;
 use pac::CorePeripherals;
 use pac::Peripherals;
-
-use hal::gpio::v2::Input;
-use hal::gpio::v2::Output;
-use hal::gpio::v2::PullUp;
-use hal::gpio::v2::PushPull;
-use hal::gpio::v2::PA02;
-use hal::gpio::v2::PA03;
+use usb_device::bus::UsbBusAllocator;
+use usb_device::prelude::*;
 
 use keyberon::action::k;
 use keyberon::debounce::Debouncer;
-// use keyberon::key_code::KbHidReport;
-// use keyberon::key_code::KeyCode;
+use keyberon::key_code::KbHidReport;
 use keyberon::key_code::KeyCode::Grave;
 use keyberon::layout::Layout;
 use keyberon::matrix::Matrix;
 use keyberon::matrix::PressedKeys;
 
-// type UsbClass = HidClass<'static, UsbBus, Keyboard<()>>;
-// type UsbClass = keyberon::Class<'static, UsbBus, ()>;
-// type UsbDev = UsbDevice<'static, UsbBus>;
-
-pub struct Cols(pub hal::gpio::v2::pin::Pin<PA02, Input<PullUp>>);
-impl_heterogenous_array! {
-    Cols,
-    dyn InputPin<Error = Infallible>,
-    U1,
-    [0]
-}
-
-pub struct Rows(pub hal::gpio::v2::pin::Pin<PA03, Output<PushPull>>);
-impl_heterogenous_array! {
-    Rows,
-    dyn OutputPin<Error = Infallible>,
-    U1,
-    [0]
-}
 pub static LAYERS: keyberon::layout::Layers = &[&[&[k(Grave)]]];
 
 #[entry]
@@ -86,14 +51,17 @@ fn main() -> ! {
     unsafe {
         USB_CLASS = Some(keyberon::new_class(bus_allocator, ()));
         USB_DEV = Some(keyberon::new_device(bus_allocator));
-        LAYOUT = Some(Layout::new(LAYERS));
-        DEBOUNCER = Some(Debouncer::new(
-            PressedKeys::default(),
-            PressedKeys::default(),
-            5,
-        ));
-        MATRIX = Matrix::new(, PA03).ok();
     }
+    let mut debouncer: Debouncer<PressedKeys<1, 1>> =
+        Debouncer::new(PressedKeys::default(), PressedKeys::default(), 5);
+
+    let mut matrix = Matrix::new(
+        [pins.analog.a0.into_pull_up_input(); 1],
+        [pins.analog.a1.into_push_pull_output(); 1],
+    )
+    .unwrap();
+
+    let mut layout = Layout::new(LAYERS);
 
     unsafe {
         core.NVIC.set_priority(interrupt::USB, 1);
@@ -101,6 +69,11 @@ fn main() -> ! {
     }
 
     loop {
+        for event in debouncer.events(matrix.get().unwrap()) {
+            layout.event(event);
+        }
+        let report: KbHidReport = layout.keycodes().collect();
+        unsafe { while let Ok(0) = USB_CLASS.as_mut().unwrap().write(report.as_bytes()) {} }
         wfi();
     }
 }
@@ -108,30 +81,15 @@ fn main() -> ! {
 static mut USB_ALLOCATOR: Option<UsbBusAllocator<UsbBus>> = None;
 static mut USB_CLASS: Option<keyberon::Class<'static, UsbBus, ()>> = None;
 static mut USB_DEV: Option<UsbDevice<UsbBus>> = None;
-static mut LAYOUT: Option<keyberon::layout::Layout> = None;
-static mut DEBOUNCER: Option<Debouncer<PressedKeys<U1, U1>>> = None;
-static mut MATRIX: Option<Matrix<Cols, Rows>> = None;
 
 fn poll_usb() {
     unsafe {
         USB_DEV.as_mut().map(|usb_dev| {
             USB_CLASS.as_mut().map(|keyboard| {
                 usb_dev.poll(&mut [keyboard]);
-
-                for event in DEBOUNCER
-                    .as_mut()
-                    .unwrap()
-                    .events(MATRIX.as_mut().unwrap().get().unwrap())
-                {
-                    let report: KbHidReport = LAYOUT.as_mut().unwrap().event(event).collect();
-                    while let Ok(0) = keyboard.write(report.as_bytes()) {}
-                }
-                // let report: KbHidReport = LAYOUT.as_mut().unwrap().tick().collect();
-                // while let Ok(0) = keyboard.write(report.as_bytes()) {}
             });
         });
     };
-    // unsafe { DEBOUNCER.unwrap().events(MATRIX.unwrap().get().unwrap()) };
 }
 
 #[interrupt]
