f583d7eac6dda8c32f1f29e377cc169afc4f292e
Stephen Onnen <stephen.onnen@gmail.com>
2021-08-25 19:47:15 -0500

    Added matrix

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use bsp::hal::gpio::v2::Pin;
 use panic_halt as _;
 
 use core::convert::Infallible;
@@ -22,8 +23,6 @@ use pac::interrupt;
 use pac::CorePeripherals;
 use pac::Peripherals;
 
-use generic_array::typenum::U1;
-
 use hal::gpio::v2::Input;
 use hal::gpio::v2::Output;
 use hal::gpio::v2::PullUp;
@@ -32,35 +31,33 @@ use hal::gpio::v2::PA02;
 use hal::gpio::v2::PA03;
 
 use keyberon::action::k;
-// use keyberon::debounce::Debouncer;
-use keyberon::impl_heterogenous_array;
-// use keyberon::matrix;
+use keyberon::debounce::Debouncer;
 // use keyberon::key_code::KbHidReport;
 // use keyberon::key_code::KeyCode;
 use keyberon::key_code::KeyCode::Grave;
-// use keyberon::layout::Layout;
-// use keyberon::matrix::Matrix;
-// use keyberon::matrix::PressedKeys;
+use keyberon::layout::Layout;
+use keyberon::matrix::Matrix;
+use keyberon::matrix::PressedKeys;
 
 // type UsbClass = HidClass<'static, UsbBus, Keyboard<()>>;
 // type UsbClass = keyberon::Class<'static, UsbBus, ()>;
 // type UsbDev = UsbDevice<'static, UsbBus>;
 
 pub struct Cols(pub hal::gpio::v2::pin::Pin<PA02, Input<PullUp>>);
-impl_heterogenous_array! {
-    Cols,
-    dyn InputPin<Error = Infallible>,
-    U1,
-    [0]
-}
+// impl_heterogenous_array! {
+//     Cols,
+//     dyn InputPin<Error = Infallible>,
+//     U1,
+//     [0]
+// }
 
 pub struct Rows(pub hal::gpio::v2::pin::Pin<PA03, Output<PushPull>>);
-impl_heterogenous_array! {
-    Rows,
-    dyn OutputPin<Error = Infallible>,
-    U1,
-    [0]
-}
+// impl_heterogenous_array! {
+//     Rows,
+//     dyn OutputPin<Error = Infallible>,
+//     U1,
+//     [0]
+// }
 pub static LAYERS: keyberon::layout::Layers = &[&[&[k(Grave)]]];
 
 #[entry]
@@ -86,17 +83,17 @@ fn main() -> ! {
     unsafe {
         USB_CLASS = Some(keyberon::new_class(bus_allocator, ()));
         USB_DEV = Some(keyberon::new_device(bus_allocator));
-        // LAYOUT = Some(Layout::new(LAYERS));
-        // DEBOUNCER = Some(Debouncer::new(
-        //     PressedKeys::default(),
-        //     PressedKeys::default(),
-        //     5,
-        // ));
-        // MATRIX = Matrix::new(
-        //     Cols(pins.analog.a0.into_pull_up_input()),
-        //     Rows(pins.analog.a1.into_push_pull_output()),
-        // )
-        // .ok();
+        LAYOUT = Some(Layout::new(LAYERS));
+        DEBOUNCER = Some(Debouncer::new(
+            PressedKeys::default(),
+            PressedKeys::default(),
+            5,
+        ));
+        MATRIX = Matrix::new(
+            [pins.analog.a0.into_pull_up_input(); 1],
+            [pins.analog.a1.into_push_pull_output(); 1],
+        )
+        .ok();
     }
 
     unsafe {
@@ -110,24 +107,29 @@ fn main() -> ! {
 }
 
 static mut USB_ALLOCATOR: Option<UsbBusAllocator<UsbBus>> = None;
-// static mut USB_BUS: Option<UsbDevice<UsbBus>> = None;
 static mut USB_CLASS: Option<keyberon::Class<'static, UsbBus, ()>> = None;
 static mut USB_DEV: Option<UsbDevice<UsbBus>> = None;
-// static mut LAYOUT: Option<keyberon::layout::Layout> = None;
-// static mut DEBOUNCER: Option<Debouncer<PressedKeys<U1, U1>>> = None;
-// static mut MATRIX: Option<Matrix<Cols, Rows>> = None;
+static mut LAYOUT: Option<keyberon::layout::Layout> = None;
+static mut DEBOUNCER: Option<Debouncer<PressedKeys<1, 1>>> = None;
+static mut MATRIX: Option<Matrix<Pin<PA02, Input<PullUp>>, Pin<PA03, Output<PushPull>>, 1, 1>> =
+    None;
 
 fn poll_usb() {
     unsafe {
         USB_DEV.as_mut().map(|usb_dev| {
-            USB_CLASS.as_mut().map(|usb_class| {
-                usb_dev.poll(&mut [usb_class]);
+            USB_CLASS.as_mut().map(|keyboard| {
+                usb_dev.poll(&mut [keyboard]);
 
+                // for event in DEBOUNCER
+                //     .as_mut()
+                //     .unwrap()
+                //     .events(MATRIX.as_mut().unwrap().get().unwrap())
+                // {
+                //     let report: KbHidReport = LAYOUT.as_mut().unwrap().event(event).collect();
+                //     while let Ok(0) = keyboard.write(report.as_bytes()) {}
+                // }
                 // let report: KbHidReport = LAYOUT.as_mut().unwrap().tick().collect();
-                // while let Ok(0) = usb_class.write(report.as_bytes()) {}
-                // if let Ok(count) = serial.read(&mut buf) {
-                //     serial.write(&buf[..count]).ok();
-                // };
+                // while let Ok(0) = keyboard.write(report.as_bytes()) {}
             });
         });
     };
